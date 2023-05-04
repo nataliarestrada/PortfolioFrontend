697733fa0ae32779a5867ccacbcca0f610633e82
nataliarestrada <nati.tachito@gmail.com>
2023-05-04 19:34:45 -0300

    educacion, experiencia, skills y proyectos

--- Portfolio-FrontEnd/src/app/components/proyectos/proyectos.component.ts
+++ Portfolio-FrontEnd/src/app/components/proyectos/proyectos.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Proyecto {
+  nombre: string;
+  fecha: string;
+  descripcion: string;
+  imagen: string;
+  link: string;
+}
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent {
-
+  proyectos: Proyecto[] = [
+    {
+      nombre: 'Proyecto 1',
+      fecha: 'Enero 2022',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imagen: 'ruta/a/la/imagen',
+      link: 'link/a/la/evidencia'
+    },
+    {
+      nombre: 'Proyecto 2',
+      fecha: 'Febrero 2022',
+      descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imagen: 'ruta/a/la/imagen',
+      link: 'link/a/la/evidencia'
+    },
+    // Agregar más proyectos aquí
+  ];
 }
